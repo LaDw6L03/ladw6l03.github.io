9049c6103997ab3857b6c5830c7dd4b5f886bbb7
LaDw6L03 <axeldugat23@gmail.com>
2026-06-09 09:05:01 +0200

    Revert "Auto stash before merge of "main" and "origin/main""
    This reverts commit 28f667f04eca2fbc6fd1b2d699203f57a5b1b707.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ladw6l03.github.io',
-  integrations: [mdx(), sitemap()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  integrations: [mdx(), sitemap(), tailwind()]
 });
+
+
